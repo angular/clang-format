7da1b6897616307179728071638f29c0986a28eb
Martin Probst <martin@probst.io>
2015-04-13 16:21:45 -0700

    Primitive test for relative and absolute paths.

--- test.sh
+++ test.sh
@@ -7,3 +7,7 @@ if [[ "$ACTUAL" = "$EXPECTED" ]]; then
   echo "Expected $EXPECTED, got $ACTUAL" >&2
   exit 1
 fi
+
+# Make sure we can run on relative an absolute paths (set -e checks for errors).
+/usr/bin/env node index.js index.js &>/dev/null
+/usr/bin/env node index.js $PWD/index.js &>/dev/null
