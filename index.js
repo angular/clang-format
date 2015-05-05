e1bcde2c2190b1a6006d639491ac4c06e0c0f265
Martin Probst <martin@probst.io>
2015-05-05 08:18:45 -0700

    Remove obsolete comment on indirection.

--- index.js
+++ index.js
@@ -40,10 +40,7 @@ module.exports.spawnClangFormat = spawnClangFormat;
 
 if (require.main === module) {
   try {
-    // This indirection is needed so that __dirname does not point to the location of the symlink'ed
-    // executable but to the js file itself, so that the binaries in /bin/ can be resolved relative
-    // to that.
-    clangFormat.spawnClangFormat(process.argv.slice(2), process.exit, 'inherit');
+    spawnClangFormat(process.argv.slice(2), process.exit, 'inherit');
   } catch (e) {
     process.stdout.write(e.message);
     process.exit(1);
