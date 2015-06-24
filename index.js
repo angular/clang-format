2f9ceb9c3200e0ea3eeb6f8191ed28f4265ff923
Martin Probst <martin@probst.io>
2015-06-23 20:27:27 -0700

    feat: load project specific clang-format versions.
    This makes it easier for projects to enforce formatting with a specific
    clang-format version.
    
    Uses node-resolve to find a clang-format relative to (in order) the last
    given file to format, then the current working directory. After that,
    falls back to the clang-format that came with this node module.
    
    This is not using Liftoff; it makes too many assumptions on the project
    type, e.g. looking for a configuration file a la `gulpfile.js`.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
-var spawn = require('child_process').spawn;
-var os = require('os');
 var fs = require('fs');
+var os = require('os');
+var path = require('path');
+var resolve = require('resolve').sync;
+var spawn = require('child_process').spawn;
 
 /**
  * Start a child process running the native clang-format binary.
@@ -12,14 +14,23 @@ var fs = require('fs');
  * @returns {Stream} the formatted code
  */
 function clangFormat(file, enc, style, done) {
-  return spawnClangFormat(['-style=' + style, file.path], done, ['ignore', 'pipe', process.stderr])
-      .stdout;
+  var args = ['-style=' + style, file.path];
+  return spawnClangFormat(args, done, ['ignore', 'pipe', process.stderr]).stdout;
 }
 
 /**
- * Spawn the clang-format binary with given arguments
+ * Spawn the clang-format binary with given arguments.
  */
 function spawnClangFormat(args, done, stdio) {
+  // WARNING: This function's interface should stay stable across versions for the cross-version
+  // loading below to work.
+  if (args.indexOf('-version') !== -1 || args.indexOf('--version') !== -1) {
+    // Print our version.
+    // This makes it impossible to format files called '-version' or '--version'. That's a feature.
+    // minimist & Co don't support single dash args, which we need to match binary clang-format.
+    console.log('clang-format NPM version', require('./package.json').version, 'at', __filename);
+    process.exit(0);
+  }
   var nativeBinary;
   if (os.platform() === 'win32') {
     nativeBinary = __dirname + '/bin/win32/clang-format.exe';
@@ -32,21 +43,47 @@ function spawnClangFormat(args, done, stdio) {
               "Consider installing it with your native package manager instead.\n";
     throw new Error(message);
   }
-  var child_process = spawn(nativeBinary, args, {stdio: stdio});
-  child_process.on('close', function(exit) {
+  var clangFormatProcess = spawn(nativeBinary, args, {stdio: stdio});
+  clangFormatProcess.on('close', function(exit) {
     if (exit) done(exit);
   });
-  return child_process;
+  return clangFormatProcess;
 }
 
-module.exports = clangFormat;
-module.exports.spawnClangFormat = spawnClangFormat;
-
-if (require.main === module) {
+function main() {
+  // Find clang-format in node_modules of the project of the .js file, or cwd.
+  var nonDashArgs = process.argv.filter(function(arg, idx) { return idx > 1 && arg[0] != '-' });
+  // Using the last file makes it less likely to collide with clang-format's argument parsing.
+  var lastFileArg = nonDashArgs[nonDashArgs.length - 1];
+  var basedir = lastFileArg ? path.dirname(lastFileArg) :  // relative to the last .js file given.
+                    process.cwd();                         // or relative to the cwd()
+  var resolvedClangFormat;
+  var clangFormatLocation;
   try {
-    spawnClangFormat(process.argv.slice(2), process.exit, 'inherit');
+    clangFormatLocation = resolve('clang-format', {basedir: basedir});
+    resolvedClangFormat = require(clangFormatLocation);
+  } catch (e) {
+    // Ignore and use the clang-format that came with this package.
+  }
+  var actualSpawnFn;
+  if (!resolvedClangFormat) {
+    actualSpawnFn = spawnClangFormat;
+  } else if (resolvedClangFormat.spawnClangFormat) {
+    actualSpawnFn = resolvedClangFormat.spawnClangFormat;
+  } else {
+    throw new Error('Incompatible clang-format loaded from ' + clangFormatLocation);
+  }
+  // Run clang-format.
+  try {
+    // Pass all arguments to clang-format, including e.g. -version etc.
+    actualSpawnFn(process.argv.slice(2), process.exit, 'inherit');
   } catch (e) {
     process.stdout.write(e.message);
     process.exit(1);
   }
 }
+
+if (require.main === module) main();
+
+module.exports = clangFormat;
+module.exports.spawnClangFormat = spawnClangFormat;
